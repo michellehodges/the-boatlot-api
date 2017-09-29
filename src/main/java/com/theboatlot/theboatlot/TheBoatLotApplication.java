6e0eb49b9e0a091e0be6d8eed2ef97b45220cf53
michellehodges <mischamangan@gmail.com>
2017-09-29 11:52:25 -0400

    initial commit

---
+++ src/main/java/com/theboatlot/theboatlot/TheBoatLotApplication.java
@@ -0,0 +1,12 @@
+package com.theboatlot.theboatlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBoatLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBoatLotApplication.class, args);
+	}
+}
